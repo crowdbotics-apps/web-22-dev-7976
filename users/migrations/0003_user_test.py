2306e40cf278bc09c769043a78304fae0de9bb5b
Crowdbotics <team@crowdbotics.com>
2020-07-22 07:44:16 +0000

    Update models

--- users/migrations/0003_user_test.py
+++ users/migrations/0003_user_test.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('users', '0002_auto_20200722_0557'),
+        ("users", "0002_auto_20200722_0557"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='user',
-            name='test',
+            model_name="user",
+            name="test",
             field=models.BigIntegerField(blank=True, null=True),
         ),
     ]
